3ce7503d8e63836484a09bc2620b883a53641850
Andrey Karamnoff <karamnoff@gmail.com>
2020-06-25 18:50:05 +0300

    add solution for 04-side-effects

--- __tests__/buildUser.test.js
+++ __tests__/buildUser.test.js
@@ -5,5 +5,17 @@ import getFunction from '../functions.js';
 const buildUser = getFunction();
 
 // BEGIN (write your solution here)
+test('buildUser random', () => {
+  const user1 = buildUser();
+  const user2 = buildUser();
+  expect(user1).not.toEqual(user2);
+});
 
+test('buildUser override', () => {
+  const newData1 = {
+    email: 'test@email.com',
+  };
+  const user1 = buildUser(newData1);
+  expect(user1).toMatchObject(newData1);
+});
 // END
