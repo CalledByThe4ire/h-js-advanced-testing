0654de826665320aeb3bd079a0f821aea07e7632
Andrey Karamnoff <karamnoff@gmail.com>
2020-06-25 17:27:49 +0300

    add solution for 02-errors

--- __tests__/read.test.js
+++ __tests__/read.test.js
@@ -5,5 +5,8 @@ import getFunction from '../functions.js';
 const read = getFunction();
 
 // BEGIN (write your solution here)
-
+test('read', () => {
+  expect(() => read('/undefined')).toThrow();
+  expect(() => read('/etc')).toThrow();
+});
 // END
