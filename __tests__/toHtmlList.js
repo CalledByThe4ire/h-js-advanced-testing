88d5884fb02f67fca9563de6810d76b3cf7a2245
Andrey Karamnoff <karamnoff@gmail.com>
2020-06-25 17:37:26 +0300

    add solution for 03-fixtures

--- __tests__/toHtmlList.js
+++ __tests__/toHtmlList.js
@@ -7,5 +7,19 @@ import getFunction from '../functions.js';
 const toHtmlList = getFunction();
 
 // BEGIN (write your solution here)
+const getFixturePath = (filename) =>
+  path.join(path.resolve(), '__fixtures__', filename);
+const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
 
+let expected;
+const formats = ['csv', 'json', 'yml'];
+
+beforeAll(async () => {
+  expected = (await readFile('result.html')).toString().trim();
+});
+
+test.each(formats)('to HTML List', async (format) => {
+  const actual = await toHtmlList(getFixturePath(`list.${format}`));
+  expect(actual).toBe(expected);
+});
 // END
