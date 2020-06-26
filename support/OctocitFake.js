079b32a52e4379592d5e80a27709ac0659777b45
Andrey Karamnoff <karamnoff@gmail.com>
2020-06-26 16:56:29 +0300

    add solution for 07-stub

--- support/OctocitFake.js
+++ support/OctocitFake.js
@@ -1,5 +1,13 @@
 // @ts-check
 
 export default class OctokitFake {
-  //
+  constructor(data) {
+    this.data = data;
+  }
+
+  repos = {
+    listForUser: () => (
+      Promise.resolve({ data: this.data }) // так как метод асинхронный
+    ),
+  }
 }
