b3a94b5d59edf5aea7bd9d3b360fccf25eda66e9
Andrey Karamnoff <karamnoff@gmail.com>
2020-06-26 14:32:56 +0300

    add solution for 06-elimination-of-side-effects

--- __tests__/getFilesCount.test.js
+++ __tests__/getFilesCount.test.js
@@ -9,5 +9,12 @@ const getFilesCount = getFunction();
 const getFixturePath = (name) => path.join('__fixtures__', name);
 
 // BEGIN (write your solution here)
-
+describe('dependency injection', () => {
+  test('flat', () => {
+    expect(getFilesCount(getFixturePath('flat'), () => {})).toBe(3);
+  });
+  test('nested', () => {
+    expect(getFilesCount(getFixturePath('nested'), () => {})).toBe(4);
+  });
+});
 // END
