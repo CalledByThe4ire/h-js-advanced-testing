e775a5d4611bc1b38ba5f6e29e8f8a3e256297d3
Andrey Karamnoff <karamnoff@gmail.com>
2020-06-26 12:44:40 +0300

    add solution for 05-files

--- __tests__/prettifyHTMLFile.test.js
+++ __tests__/prettifyHTMLFile.test.js
@@ -8,5 +8,25 @@ import getFunction from '../functions.js';
 const prettifyHTMLFile = getFunction();
 
 // BEGIN (write your solution here)
+const getFixturePath = (name) => path.join('__fixtures__', name);
 
+const filename = 'before.html';
+const dest = path.join(os.tmpdir(), filename);
+const src = getFixturePath(filename);
+
+let expected;
+
+beforeAll(async () => {
+  expected = await fs.readFile(getFixturePath('after.html'), 'utf-8');
+});
+
+beforeEach(async () => {
+  await fs.copyFile(src, dest);
+});
+
+test('prettifyHTMLFile', async () => {
+  await prettifyHTMLFile(dest);
+  const actual = await fs.readFile(dest, 'utf-8');
+  expect(actual).toBe(expected);
+});
 // END
