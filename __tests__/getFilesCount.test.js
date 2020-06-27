182423a940fe0b59639ed840bda0631309314c6d
Andrey Karamnoff <karamnoff@gmail.com>
2020-06-27 08:54:31 +0300

    add solution for 09-mocking

--- __tests__/getFilesCount.test.js
+++ __tests__/getFilesCount.test.js
@@ -9,5 +9,11 @@ const getFilesCount = getFunction();
 const getFixturePath = (name) => path.join('__fixtures__', name);
 
 // BEGIN (write your solution here)
-
+test('getFilesCount', () => {
+  const directoryPath = getFixturePath('nested');
+  const mock = jest.fn();
+  getFilesCount(directoryPath, mock);
+  expect(mock).toHaveBeenCalledTimes(1);
+  expect(mock).toHaveBeenCalledWith('Go!');
+});
 // END
